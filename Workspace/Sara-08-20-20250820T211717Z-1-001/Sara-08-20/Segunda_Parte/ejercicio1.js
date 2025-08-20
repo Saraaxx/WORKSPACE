223d58bc43bd9f228d5ad2d636fcb9b5981e7d2c
Saraaxx <saraaguirre2210@gmail.com>
2025-08-20 16:18:37 -0500

    Add files via upload

---
+++ Workspace/Sara-08-20-20250820T211717Z-1-001/Sara-08-20/Segunda_Parte/ejercicio1.js
@@ -0,0 +1,2 @@
+const mensError = document.querySelector('h1');
+mensError.innerHTML = ('Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.')
---
+++ Workspace/Sara-08-20-20250820T211717Z-1-001/Sara-08-20/Segunda_Parte/ejercicio3.js
@@ -0,0 +1,2 @@
+const holaMundo = document.querySelector('h1');
+holaMundo.innerHTML = ('Hola Mundo')
